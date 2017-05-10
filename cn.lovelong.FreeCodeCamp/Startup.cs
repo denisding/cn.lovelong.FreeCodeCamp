b5f720e1811bb671b913ec4a2c8997830b23842c
Denis Ding <yyguangzheng@163.com>
2017-05-10 21:47:37 +0800

    初始化库

---
+++ cn.lovelong.FreeCodeCamp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cn.lovelong.FreeCodeCamp.Startup))]
+namespace cn.lovelong.FreeCodeCamp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
